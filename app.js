09eab42f2377468152ea78f84d82aa26229bbf51
nadeeraka <nimanthanadeeraka@outlook.com>
2019-05-15 12:14:25 +0530

    2st commit

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const app = express();
+const bodyparser = require("body-parser");
+
+app.use(bodyparser.urlencoded({ extended: false }));
+// using bodyparser to extrate data that in the request body
 
 app.use((req, res, next) => {
   console.log("1 st middleware");
