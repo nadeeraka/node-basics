551aa229e340465f63b9d207a4aeccab61a38bd9
nadeeraka <nimanthanadeeraka@outlook.com>
2019-05-16 12:04:07 +0530

    6th commit

--- routes/shop.js
+++ routes/shop.js
@@ -5,8 +5,16 @@ const router = express();
 router.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../", "views", "shop.html"));
 });
+router.get("/shop", (req, res) => {
+  res.sendFile(path.join(__dirname, "../", "views", "shop.html"));
+});
 router.get("/add-product", (req, res) => {
   res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
+});
+
+router.post("/add-product", (req, res) => {
+  console.log(req.body);
+  res.redirect("/shop");
 });
 
 module.exports = router;
